b55727b11b6b37a51f03ea56739e4337ac35af32
Luiz Fernando <99697056+Lui8silva@users.noreply.github.com>
2022-03-17 22:49:52 +0000

    Exercicio 2

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app = Flask('app')
 @app.route('/')
 def index():
   return '<h1>Olá Mundo!</h1>'
+@app.route('/unifran')
+def unifran():
+  return '<h2>Universidade de Franca</h2>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
