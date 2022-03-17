18a0f8633b6bd067d0b0f5e9c03a7008fc0970d7
Luiz Fernando <99697056+Lui8silva@users.noreply.github.com>
2022-03-17 22:43:52 +0000

    Correcao da frase

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask
 app = Flask('app')
 
 @app.route('/')
-def hello_world():
-  return '<h1>Hello, World!</h1>'
+def index():
+  return '<h1>Minha primeira rota!</h1>'
 
-app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
